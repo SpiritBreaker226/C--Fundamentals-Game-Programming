a73dce8a7344e8def59f5bc0a4d5072a47ab43f7
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-08 10:05:40 -0800

    Add Hello Work For C++

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+
+int main(int argc, const char** argv) {
+  printf("Hello, my name is Jason\n");
+  printf("I'm from Toronto, Ontario, Canada\n");
+  printf("I enjoy hiking\n");
+
+  return 0;
+}
