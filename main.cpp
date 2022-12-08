6e0d34b46dd3022dea41ce1c0495f1ff6487ecd4
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-08 10:35:17 -0800

    Add Comments to Hello World Main Function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <cstdio>
 
+/**
+ * source file: main.cpp
+ * description: displays author's name, hometown and hobby
+ * author: Jason Stathopulos
+*/
 int main(int argc, const char** argv) {
+  // name of author
   printf("Hello, my name is Jason\n");
+
+  // author's hometown
   printf("I'm from Toronto, Ontario, Canada\n");
+
+  // author's hobby
   printf("I enjoy hiking\n");
 
   return 0;
