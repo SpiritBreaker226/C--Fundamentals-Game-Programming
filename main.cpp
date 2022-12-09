e9a02b12326bc1fa74b0a96a7b235e666d473dbd
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-09 09:57:38 -0800

    Add Velocity Lesson

--- main.cpp
+++ main.cpp
@@ -8,6 +8,14 @@ int main()
 
   InitWindow(windowWidth, windowHeight, "Dapper Dasher!");
 
+  // rectangle dimensions
+  const int width{50};
+  const int height{80};
+
+  // places the rectangle on the ground
+  int posY{windowHeight - height};
+  int velocity{0};
+
   SetTargetFPS(60);
   while (!WindowShouldClose())
   {
@@ -16,6 +24,15 @@ int main()
     ClearBackground(WHITE);
 
     // Start Game Logic
+
+    if (IsKeyPressed(KEY_SPACE))
+    {
+      velocity -= 10;
+    }
+
+    posY += velocity;
+
+    DrawRectangle(windowWidth / 2, posY, width, height, BLUE);
 
     // End Game Logic
 
