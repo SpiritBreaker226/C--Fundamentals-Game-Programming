75abc569dbe1017328b77f96d267acc6301a8f98
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-09 10:48:39 -0800

    Add Jumping Lesson For Dasher

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@ int main()
 
   InitWindow(windowWidth, windowHeight, "Dapper Dasher!");
 
+  // acceleration due to gravity (pixels/frame)/frame
+  const int gravity{1};
+
   // rectangle dimensions
   const int width{50};
   const int height{80};
@@ -24,6 +27,18 @@ int main()
     ClearBackground(WHITE);
 
     // Start Game Logic
+
+    // perform ground check
+    if (posY >= (windowHeight - height))
+    {
+      // rectangle the ground
+      velocity = 0;
+    }
+    else
+    {
+      // rectangle in the air
+      velocity += gravity;
+    }
 
     if (IsKeyPressed(KEY_SPACE))
     {
