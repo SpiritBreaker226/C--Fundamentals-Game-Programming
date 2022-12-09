be7707f3bf99d2bd36b333b2cdd0b3546ddb40ab
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-08 16:47:31 -0800

    Add Draw to Windown Lesson

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include "raylib.h"
 
-int main(int argc, const char **argv)
+int main()
 {
   int width = 350;
 
   // create a window
   InitWindow(width, 900, "Jason Stathopulos");
 
-  return 0;
+  while (true)
+  {
+    // init the drawing to the window
+    BeginDrawing();
+
+    // clears the background to prevent flickering,
+    // and want to avoid causing sizes.
+    ClearBackground(RED);
+
+    // treadown the window
+    EndDrawing();
+  }
 }
