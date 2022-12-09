eec429001bc72b1b0ae3ffd336c5636f2b962c11
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-08 16:08:58 -0800

    Add Examples of Assign Data Type Lesson

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include "raylib.h"
 
-int main(int argc, const char** argv) {
+int main(int argc, const char **argv)
+{
+  float root_beer = 1.99;
+  double cheese_burger{5.99};
+  bool should_have_lunch{};
+
   int width = 350;
 
   // create a window
