e249af4a9717ed4d738bcba1c8f6631c412172ea
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-08 16:17:16 -0800

    Remove Comparion Operators Lesson

--- main.cpp
+++ main.cpp
@@ -2,24 +2,6 @@
 
 int main(int argc, const char **argv)
 {
-  // false
-  bool equal{4 == 9};
-
-  // true
-  bool not_equal{4 != 9};
-
-  // true
-  bool less{4 < 9};
-
-  // false
-  bool greater{4 > 9};
-
-  // true
-  bool lest_equal{4 <= 9};
-
-  // false
-  bool greater_equal{4 >= 9};
-
   int width = 350;
 
   // create a window
