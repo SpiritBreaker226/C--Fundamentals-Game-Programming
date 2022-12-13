983df138666efaf6400d1514c2d5057ec7cfddbe
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-12 16:47:10 -0800

    Add Vector & Movment Lesson

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "raymath.h"
 
 int main()
 {
@@ -9,6 +10,8 @@ int main()
   InitWindow(windowWidth, windowHeight, "Classy Clash!");
   // Map
   Texture2D map = LoadTexture("./nature_tileset/OpenWorldMap24x24.png");
+  Vector2 mapPos{0.0, 0.0};
+  const float speed = 4.0;
 
   SetTargetFPS(60);
   while (!WindowShouldClose())
@@ -18,7 +21,23 @@ int main()
 
     // Start Game Logic
 
-    Vector2 mapPos{0.0, 0.0};
+    // direction is the oppose way you want to go
+    Vector2 direction{};
+    if (IsKeyDown(KEY_A))
+      direction.x -= 1.0;
+    if (IsKeyDown(KEY_D))
+      direction.x += 1.0;
+    if (IsKeyDown(KEY_W))
+      direction.y -= 1.0;
+    if (IsKeyDown(KEY_S))
+      direction.y += 1.0;
+
+    if (Vector2Length(direction) != 0.0)
+    {
+      // set map = mapPos - direction (or movement vector)
+      mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+    }
+
     DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
     // End Game Logic
